56ab1b0f7bcd12e00a6cb80b8891a5073c569929
Nofido189 <nogatiya@gmail.com>
2022-12-13 21:12:02 +0200

    ex1: Shortened the code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,33 +9,46 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.MoveWithConstantPower;
 import frc.robot.subsystems.Chassis;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final Chassis chassis = new Chassis();
+  private final Chassis chassis;
 
-  private final MoveWithConstantPower autonomusCommand = new MoveWithConstantPower(0.2, chassis);
+  private final MoveWithConstantPower autonomusCommand;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
+    chassis = new Chassis(Constants.PORT_NUMBER_FRONTRIGHT, Constants.PORT_NUMBER_REARRIGHT,
+    Constants.PORT_NUMBER_FRONTLEFT, Constants.PORT_NUMBER_REARLEFT);
+    autonomusCommand = new MoveWithConstantPower(0.2, chassis);
     configureButtonBindings();
 
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
